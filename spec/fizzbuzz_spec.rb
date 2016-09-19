ae9e10bd7dacf5ffbf3527753c0c2f403cced759
Laura Weston <laura.weston@outlook.com>
2016-09-19 12:00:35 +0100

    fizzbuzz returns 'fizzbuzz' for multiples of 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -17,8 +17,12 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
 
-  it 'returns "buzz" when pass 10' do
+  it 'returns "buzz" when passed 10' do
     expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 
 end
